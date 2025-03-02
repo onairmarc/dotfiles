6e79968a57e2f2e302bfa9fef784e89b445dc3f6
Marc Beinder <marc@marcbeinder.com>
2025-03-02 09:23:10 -0600

    add and organize colors

--- .config/color.sh
+++ .config/color.sh
@@ -1,6 +1,10 @@
 # Colors
-export COL_GREEN='\033[0;32m'
-export COL_RED='\033[1;31m'
+export COL_RED="\033[1;31m"
+export COL_GREEN="\033[0;32m"
+export COL_YELLOW="\x1b[33m"
+export COL_BLUE="\x1b[34m"
+export COL_MAGENTA="\x1b[35m"
+export COL_CYAN="\x1b[36m"
 
 # Reset color
-export COL_RESET='\033[0m'
+export COL_RESET="\033[0m"
