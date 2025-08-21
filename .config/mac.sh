ff6217dd395f86c1fd60f14b65e088c99a1b82d8
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-08-21 12:26:59 -0500

    update xdebug_mode

--- .config/mac.sh
+++ .config/mac.sh
@@ -11,4 +11,4 @@ export NODE_ENV="dev"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export XDEBUG_MODE="coverage"
+export XDEBUG_MODE="coverage,debug,develop"
