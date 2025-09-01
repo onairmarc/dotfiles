14084ad5c489879c6a8f060b8e390b1f4d68ce3a
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-09-01 11:12:03 -0500

    update mac startup script

--- .startup/mac.sh
+++ .startup/mac.sh
@@ -1,3 +1,13 @@
 osascript -e 'tell application "OpenVPN Connect" to launch'
 sleep 2
-osascript -e 'tell application "System Events" to tell process "OpenVPN Connect" to set visible to false'
+osascript -e '
+tell application "System Events"
+    tell process "OpenVPN Connect"
+        set closeButton to (first button of window 1 whose subrole is "AXCloseButton")
+        set {x, y} to position of closeButton
+        set {w, h} to size of closeButton
+    end tell
+end tell
+set clickX to (x + (w / 2)) as integer
+set clickY to (y + (h / 2)) as integer
+do shell script "cliclick c:" & clickX & "," & clickY & " w:100"'
